fbc5cb75391abad2481f2625466567279d5dfaf8
Faisal <fmohd743@gmail.com>
2024-12-17 12:49:42 +0530

    basic funtionality of csharp

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿
+// Console.WriteLine("Hello, World!");
+// Console.WriteLine("This is faisal");
+
+using System.Net.Security;
+
+int x=19;
+double y=90.90;
+Console.WriteLine(x);
+int b=89;
+// Console.WriteLine(x+b);
+// Console.WriteLine(y);
+
+// Console.Write(Math.Max(14,3));
+// string hello="this is mohd faisal";
+// Console.Write(hello.ToUpper());
+
+
+//this is for taking input from user
+// Console.WriteLine("Enter the name");
+// string name=Console.ReadLine();
+// Console.WriteLine("Enter Candies");
+// string candies=Console.ReadLine();
+// Console.Write($"Hi {name} you got {candies} candies");
+
+//this ismethod
+
+static void greet(){
+    Console.WriteLine("Good Morning!!");
+
+}
+
+greet();
